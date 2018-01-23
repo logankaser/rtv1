1f39dac5a1c467e9699211c0176af3d645207a96
Damia Hill <dhill@e1z3r6p18.42.us.org>
2018-01-22 16:09:35 -0800

    added new structs to header

--- RTv1.h
+++ RTv1.h
@@ -6,12 +6,20 @@
 /*   By: lkaser <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 11:03:56 by lkaser            #+#    #+#             */
-/*   Updated: 2017/11/16 18:57:55 by lkaser           ###   ########.fr       */
+/*   Updated: 2018/01/22 16:08:39 by dhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+typedef	enum		e_type
+{
+	t_plane = 1,
+	t_sphere,
+	t_cylinder,
+	t_cone
+}					t_type;
 
 typedef struct		s_map
 {
@@ -19,5 +27,19 @@ typedef struct		s_map
 	int				width;
 	t_list			*data;
 }					t_map;
+
+typedef	struct		s_object
+{
+	t_vec3			position;
+	double			radius;
+	unsigned		color;
+}					t_obj;
+
+typedef	struct		s_light
+{
+	unsigned		color;
+	double			intensity;
+}
+
 
 #endif
