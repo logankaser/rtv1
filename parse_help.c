5b0b13966d9c6222e7e3b1bd614964b55d6f1e93
Damia Hill <dhill@e1z2r9p45.42.us.org>
2018-02-02 16:25:03 -0800

    adding error handling

--- parse_help.c
+++ parse_help.c
@@ -6,7 +6,7 @@
 /*   By: dhill <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/30 19:43:58 by dhill             #+#    #+#             */
-/*   Updated: 2018/02/02 14:48:18 by lkaser           ###   ########.fr       */
+/*   Updated: 2018/02/02 16:24:43 by dhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	get_vec3(char *p1, char *p2, char *p3, t_vec3 *var)
 
 void	parse_sphere(char **split, t_obj *obj)
 {
+	if (!split[5])
+		ft_err("Improper sphere format.");
 	obj->type = t_sphere;
 	obj->radius = ft_atof(split[4]);
 	obj->color = ft_htou(split[5]);
@@ -28,6 +30,8 @@ void	parse_sphere(char **split, t_obj *obj)
 
 void	parse_cone_plane(char **split, t_obj *obj)
 {
+	if (!split[7])
+		ft_err("Improper cone or plane format.");
 	obj->type = ft_strequ(split[0], "cone") == 1 ? t_cone : t_plane;
 	get_vec3(split[4], split[5], split[6], &obj->rotation);
 	vec3_normalize(&obj->rotation);
@@ -36,6 +40,8 @@ void	parse_cone_plane(char **split, t_obj *obj)
 
 void	parse_cylinder(char **split, t_obj *obj)
 {
+	if (!split[8])
+		ft_err("Improper cylinder format.");
 	obj->type = t_cylinder;
 	get_vec3(split[4], split[5], split[6], &obj->rotation);
 	vec3_normalize(&obj->rotation);
